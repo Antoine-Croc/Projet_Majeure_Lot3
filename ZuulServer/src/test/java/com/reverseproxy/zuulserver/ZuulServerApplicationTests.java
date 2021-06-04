1e7d195c52f85d4a6cbe5d9ec3a8b3cc192fdf03
clementcornu <clement.cornu@cpe.fr>
2021-06-04 09:40:43 +0200

    Ajout du serveur Zuul

---
+++ ZuulServer/src/test/java/com/reverseproxy/zuulserver/ZuulServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reverseproxy.zuulserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
